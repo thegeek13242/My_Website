a6835967bdc8b42834e37c797c03aa29cbaeda7a
Aviral Verma <bt20cse214@iiitn.ac.in>
2021-09-13 13:17:31 +0530

    Particle JS background Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Particles from 'react-particles-js';
+import parameters from './assets/particles.json';
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent-container">
+    <div id="particles-js">
+    <Particles params={parameters} />
+    </div>
+    <div className="container">
+    <h1 className="name">Aviral Verma</h1>
+    </div>
     </div>
   );
 }
