948ef5d87d9300a3ebc88ad558dd2ca9a16187f1
Aviral Verma <bt20cse214@iiitn.ac.in>
2021-09-14 20:03:07 +0530

    Changed color scheme & added some content

--- src/assets/NavBar.js
+++ src/assets/NavBar.js
@@ -1,20 +1,38 @@
-import './NavBar.css'
+import "./NavBar.css";
 const NavBar = () => {
-    return(
-        <nav className="nav-container">
-            <div className="nav-name">
-                <h2>Aviral Verma</h2>
-                </div>
-                    <div><a href="#about">About Me</a></div>
-                    <div><a href="#projects">Projects</a></div>
-                    <div><a href="#contact">Contact</a>
-                    <ul className="contact-list">
-                        <li><a href="https://github.com/thegeek13242" rel="noreferrer" target="_blank">GitHub</a></li>
-                        <li><a href="https://www.linkedin.com/in/aviral-verma-19b9b4201/" rel="noreferrer" target="_blank">LinkedIn</a></li>
-                    </ul>
-                </div>
-        </nav>
-    );
-}
+  return (
+    <nav className="nav-container">
+      <div className="nav-element">
+        <a className="nav-link" href="#about">About Me</a>
+      </div>
+      <div className="nav-element">
+        <a className="nav-link" href="#projects">Projects</a>
+      </div>
+      <div className="nav-element">
+        <a className="nav-link" href="#contact">Contact</a>
+        <ul className="contact-list">
+          <li>
+            <a
+              href="https://github.com/thegeek13242"
+              rel="noreferrer"
+              target="_blank"
+            >
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/aviral-verma-19b9b4201/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
