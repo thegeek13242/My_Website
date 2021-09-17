fe3d4275fd93b0a8fa5c9baea5541e8743cd3986
Aviral Verma <89376529+thegeek13242@users.noreply.github.com>
2021-09-17 18:18:57 +0530

    Added Projects Section

--- src/App.js
+++ src/App.js
@@ -12,20 +12,30 @@ const App = () => {
       <div className="container">
         <NavBar></NavBar>
         <div className="content">
-          <div className="description-container">
-            <h1 className="name">Aviral Verma</h1>
-            <p className="description">
-              Namaskar, I am Aviral Verma, a 2<sup>nd</sup> year{" "}
-              <em>Computer Science undergraduate</em> at{" "}
-              <b>Indian Institute of Information Technology, Nagpur</b>. I am
-              currently learning Full Stack Web Development and I am literally
-              open to learn about any new technology{" "}
-              <span role="img" aria-label="Smiling Face">
-                😁.
-              </span>
-              I am looking for internships in (but not limited to) the domain of
-              Web Development.
-            </p>
+          <div className="content-container">
+            <div className="description-container">
+              <h1 className="name">Aviral Verma</h1>
+              <p className="description">
+                Namaskar, I am Aviral Verma, a 2<sup>nd</sup> year{" "}
+                <em>Computer Science undergraduate</em> at{" "}
+                <b>Indian Institute of Information Technology, Nagpur</b>. I am
+                currently learning Full Stack Web Development and I am literally
+                open to learn about any new technology{" "}
+                <span role="img" aria-label="Smiling Face">
+                  😁.
+                </span>
+                I am looking for internships in (but not limited to) the domain
+                of Web Development.
+              </p>
+            </div>
+          </div>
+          <div className="project-container">
+          <section className="projects" id="projects">
+            <h2 className="title-projects">Projects</h2>
+            <div className="projects-data">
+              
+            </div>
+          </section>
           </div>
         </div>
       </div>
